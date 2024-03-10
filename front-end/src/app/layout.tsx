437b1747fd6a631c2858727fc33819cb81274907
prime <primetechnologies02@gmail.com>
2024-03-10 12:57:00 +0000

    second commit

---
+++ front-end/src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter, Kanit, Nova_Square } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
+});
+
+const kanit = Kanit({
+	subsets: ["latin"],
+	weight: "600",
+	display: "swap",
+	variable: "--font-kanit",
+});
+
+const nova = Nova_Square({
+	subsets: ["latin"],
+	weight: "400",
+	display: "swap",
+	variable: "--font-nova",
+});
+
+export const metadata: Metadata = {
+	title: "Projectile",
+	description: "A project and task management system",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${inter.variable} ${kanit.variable} ${nova.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
+}
---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="text-green p-8 font-nova">
+			<h1>Projectile</h1>
+		</main>
+	);
+}
